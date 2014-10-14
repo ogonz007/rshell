05fbbd76aaa99698edafb0ab5b845892e1e426fa
ogonz007 <ogonz007@ucr.edu>
2014-10-14 09:37:41 -0700

    added first version of rshell code

---
+++ rshell.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string in;
+	
+	while(in != "exit")
+	{
+		cout << "$ ";
+		getline(cin,in);
+		cout << endl << in << endl;
+	}
+
+
+
+
+
+
+
+
+
+	return 0;
+}
