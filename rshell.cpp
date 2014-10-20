ed260a8e9696781c4e035e2bd12725ad165271ed
Oscar Gonzalez <ogonz007@hammer.cs.ucr.edu>
2014-10-20 14:52:29 -0700

    restarted .cpp file

--- rshell.cpp
+++ rshell.cpp
@@ -1,32 +1,8 @@
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-#include <string>
 #include <iostream>
 
 using namespace std;
 
-
 int main()
 {
-	char* argv[3];
-	argv[0] = new char[3];
-	
-	//strcpy(argv[0],"ls");	
-	execvp("ls",argv);
-
-	int ret;
-
-	//char* cmd[] = { "ls", "-l", (char *)0};
-	//execvp("ls",cmd);
-
-	cout << endl << "rshell is working!" << endl;
 	return 0;
 }
-
-
-
-
-
