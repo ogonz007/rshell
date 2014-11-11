f55b6604b1bb9f6130f3181bbc8359769966aaab
Oscar Gonzalez <ogonz007@hammer.cs.ucr.edu>
2014-11-10 19:32:29 -0800

    ls.cpp recrecognizes flags and stores them in int array

---
+++ ls.cpp
@@ -0,0 +1,65 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <iostream>
+
+#define FLAG_a 1
+#define FLAG_l 2
+#define FLAG_R 3
+
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	char dirName[] = ".";
+	cout << "argc = " << argc << endl;
+	int flags[3] = {0};
+	for(int i = 0; i < argc; i++)
+	{
+		if(argv[i][0] == '-')
+		{
+			for(int j = 1; argv[i][j] != 0; j++)
+			{
+				if(argv[i][j] == 'a')
+				{
+					flags[i-1] |= FLAG_a;
+					cout << "flag = -a" << endl;
+				}
+				else if(argv[i][j] == 'l')
+				{
+					flags[i-1] |= FLAG_l;
+					cout << "flag = -l" << endl;
+				}
+				else if(argv[i][j] == 'R')
+				{
+					flags[i-1] |= FLAG_R;
+					cout << "flag = -R" << endl;
+				}
+			}
+		}
+	}
+	
+	DIR* dirp = opendir(dirName);
+	dirent* direntp;
+	
+	struct stat statbuf;
+
+	
+
+	while((direntp = readdir(dirp)))
+	{
+		stat(direntp->d_name,&statbuf);
+		cout << direntp->d_name << endl;
+	}
+	closedir(dirp);
+
+
+
+
+
+	return 0;
+}
