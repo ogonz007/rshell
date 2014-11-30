75a91cc39401da0b11f49d6a4c28462dba67381b
ogonz007 <ogonz007@ucr.edu>
2014-11-30 15:35:10 -0800

    || and && working in rshell

--- rshell.cpp
+++ rshell.cpp
@@ -19,7 +19,7 @@ using namespace std;
 ///--- prints the rshell prompt (e.g. $) ---///
 void printPrompt()
 {
-	cout << "@ ";
+	cout << "$ ";
 	return;
 }
 
@@ -35,14 +35,11 @@ string getCmdLine()
 int main()
 {
 	char delims[] = {';','|','&'};
-//	int statementPos = 0;
-//	bool good = 1;
-
 	while(1)
 	{
 		printPrompt();
 		string input = getCmdLine();
-		vector<double> connectors;
+		vector<double> connectors(1);
 		///--- search for connector types ---///
 		for(unsigned int i = 0; i < sizeof(delims); i++)
 		{
@@ -50,21 +47,23 @@ int main()
 			while((found != std::string::npos))
 			{
 				size_t pos = found + 1;
-				//cout << "input.at(" << found << ") = " << input.at(found) << endl;
 				if(input.at(found) == ';')
 					connectors.push_back(found + (SEMI_CON/10));
-				else if(input.at(found) == '&')
+				else if((input.at(found) == '&') && (input.at(found + 1) == '&'))
+				{
 					connectors.push_back(found + (AND_CON/10));
-				else if(input.at(found) == '|')
+					pos += 1;
+				}
+				else if((input.at(found) == '|') && (input.at(found + 1) == '|'))
+				{
 					connectors.push_back(found + (OR_CON/10));
+					pos += 1;
+				}
 				found = input.find(delims[i],pos);
 			}
 		}
 		sort(connectors.begin(),connectors.end());
-		/*for(unsigned int i = 0; i < connectors.size(); i++)
-		{
-			cout << "connectors[" << i << "] = " << connectors.at(i) << endl;
-		}*/
+	
 		///--- parse user input ---///
 		char* tmp_cstr = new char[input.size()+1];
 		char* saveCstr;
@@ -76,66 +75,96 @@ int main()
 		strcpy(tmp_cstr,input.c_str());
 
 		char* cstr = strtok_r(tmp_cstr,delims,&saveCstr);
+		int statementPos = 0;
+		bool good = 1;
+		int prev_connector = 0;
 
 		while(cstr != '\0')
 		{
-			if(strcmp(cstr,"exit") == 0)
+			int connector = (connectors.at(statementPos) * 10);
+			connector = connector%10;
+			if(statementPos == 0)
+				good = 1;
+			else if(connector == 1)
+				good = 1;
+			else if((connector == 2) && (prev_connector == 2) && (good == 1))
+				good = 1;
+			else if((connector == 2) && (prev_connector == 3) && (good == 0))
+				good = 1;
+			else if((connector == 3) && (prev_connector == 0) && (good == 0))
+				good = 1;
+			else if((connector == 3) && (prev_connector == 0) && (good == 1))
+				good = 0;
+			else if((connector == 3) && (prev_connector == 3) && (good == 1))
+				good = 0;
+			else if((connector == 3) && (prev_connector == 2) && (good == 0))
+				good = 1;
+			else if((connector == 3) && (prev_connector == 2) && (good == 1))
+				good = 0;
+			if(good == 1)
 			{
-				exit(0);
-			}
-		
-			pid_t pid = fork();
-			if(pid == -1)
-			{
-				perror("Error with fork()\n");
-				exit(1);
-			}
-			else if(pid == 0)
-			{
-				// child process
-
-				char** argv = new char*[input.size()+1];
-					
-				for(unsigned int i = 0; i < (input.size()+1); i++)
+				if(strcmp(cstr,"exit") == 0)
 				{
-					argv[i] = '\0';
+					exit(0);
 				}
-				
-				argv[0] = strtok(cstr," ");
-		
-				unsigned int j = 1;
 			
-				while((j < (input.size()+1)) )
+				pid_t pid = fork();
+				if(pid == -1)
 				{
-					argv[j] = strtok(NULL," ");
-					j++;
+					perror("Error with fork()\n");
+					exit(1);
 				}
+				else if(pid == 0)
+				{
+					// child process
+
+					char** argv = new char*[input.size()+1];
+						
+					for(unsigned int i = 0; i < (input.size()+1); i++)
+					{
+						argv[i] = '\0';
+					}
+					
+					argv[0] = strtok(cstr," ");
+			
+					unsigned int j = 1;
 				
-				///--- checking for commenting using # sign ---///
-				if((*argv[0] == '#'))
-				{
-					exit(0);;
+					while((j < (input.size()+1)) )
+					{
+						argv[j] = strtok(NULL," ");
+						j++;
+					}
+					
+					///--- checking for commenting using # sign ---///
+					if((*argv[0] == '#'))
+					{
+						exit(0);;
+					}
+					
+					int r = execvp(argv[0],argv);
+					if(r == -1)
+					{
+						perror("error with execvp\n");
+						good = 0;
+						exit(1);
+					}
+					good = 1;
+					delete []argv;
 				}
-				/* ADD IF STATEMENT HERE THAT WILL CHECK FOR && AND || SUCCESS BEFORE EXECVP*/
-				int r = execvp(argv[0],argv);
-				if(r == -1)
+				else if(pid > 0)
 				{
-					perror("error with execvp\n");
-					exit(1);
-				}
-				delete []argv;
-			}
-			else if(pid > 0)
-			{
-				// parent process
-				int err = wait(NULL);
-				if(err == -1)
-				{
-					perror("error with wait in parent process\n");
-					exit(1);
+					// parent process
+					int err = wait(NULL);
+					if(err == -1)
+					{
+						perror("error with wait in parent process\n");
+						exit(1);
+					}
 				}
 			}
 			cstr = strtok_r('\0',delims,&saveCstr);
+			prev_connector = connector;
+			statementPos++;
 		}
 		delete []tmp_cstr;
 	}
